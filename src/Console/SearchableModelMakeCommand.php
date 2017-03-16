a104f0fbd4fa28d780daa62e9ccd6b9f55eb0bac
ivanbabenko <babenko.i.a@gmail.com>
2017-03-16 12:04:26 +0300

    Ability to specify rule when create new searchable model

--- src/Console/SearchableModelMakeCommand.php
+++ src/Console/SearchableModelMakeCommand.php
@@ -20,23 +20,70 @@ class SearchableModelMakeCommand extends ModelMakeCommand
     {
         $options = parent::getOptions();
 
-        $options[] = ['index-configurator', 'i', InputOption::VALUE_REQUIRED, 'Specify the index configurator for the model'];
+        $options[] = ['index-configurator', 'i', InputOption::VALUE_REQUIRED,
+            'Specify the index configurator for the model. It\'ll be created if doesn\'t exist.'];
+
+        $options[] = ['search-rule', 's', InputOption::VALUE_REQUIRED,
+            'Specify the search rule for the model. It\'ll be created if doesn\'t exist.'];
 
         return $options;
     }
 
-    protected function getConfiguratorOption()
+    protected function getIndexConfigurator()
     {
         return trim($this->option('index-configurator'));
+    }
+
+    protected function getSearchRule()
+    {
+        return trim($this->option('search-rule'));
     }
 
     protected function buildClass($name)
     {
         $stub = parent::buildClass($name);
 
-        $configurator = $this->getConfiguratorOption();
-        $stub = str_replace('DummyConfigurator', $configurator ? "{$configurator}::class" : "''", $stub);
+
+        $indexConfigurator = $this->getIndexConfigurator();
+
+        if ($indexConfigurator) {
+            $stub = str_replace('DummyIndexConfigurator', "protected \$indexConfigurator = {$indexConfigurator}::class;", $stub);
+        } else {
+            $stub = preg_replace('#DummyIndexConfigurator\s+#', '', $stub);
+        }
+
+
+        $searchRule = $this->getSearchRule();
+
+        if ($searchRule) {
+            $stub = str_replace('DummySearchRules', "protected \$searchRules = [\n        {$searchRule}::class\n    ];", $stub);
+        } else {
+            $stub = preg_replace('#DummySearchRules\s+#', '', $stub);
+        }
 
         return $stub;
+    }
+
+    public function fire()
+    {
+        $indexConfigurator = $this->getIndexConfigurator();
+
+        if (!$this->alreadyExists($indexConfigurator)) {
+            $this->call('make:index-configurator', [
+                'name' => $indexConfigurator
+            ]);
+        }
+
+
+        $searchRule = $this->getSearchRule();
+
+        if (!$this->alreadyExists($searchRule)) {
+            $this->call('make:search-rule', [
+                'name' => $searchRule
+            ]);
+        }
+
+        
+        parent::fire();
     }
 }
