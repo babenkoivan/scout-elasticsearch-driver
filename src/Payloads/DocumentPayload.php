e376355d6561f7ffdf90b399a44a9b7af8c65cb4
Ilmārs Kluss <ilmars.kluss@lmt.lv>
2019-04-11 08:55:22 +0300

    use key definition only if it exists

--- src/Payloads/DocumentPayload.php
+++ src/Payloads/DocumentPayload.php
@@ -16,16 +16,20 @@ class DocumentPayload extends TypePayload
      */
     public function __construct(Model $model)
     {
-        if ($model->getKey() === null) {
-            throw new Exception(sprintf(
-                'The key value must be set to construct a payload for the %s instance.',
-                get_class($model)
-            ));
+        if ($model->getKeyName()) {
+            if ($model->getKey() === null) {
+                throw new Exception(sprintf(
+                    'The key value must be set to construct a payload for the %s instance.',
+                    get_class($model)
+                ));
+            }
         }
 
         parent::__construct($model);
 
-        $this->payload['id'] = $model->getKey();
-        $this->protectedKeys[] = 'id';
+        if ($model->getKeyName()) {
+            $this->payload['id'] = $model->getKey();
+            $this->protectedKeys[] = 'id';
+        }
     }
 }
