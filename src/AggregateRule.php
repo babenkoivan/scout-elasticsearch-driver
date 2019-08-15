a2418e180b923bb178b69a0b2f4b66a4ccb009d8
Alex Kovtunov <582969@gmail.com>
2019-08-15 14:48:39 +0400

    AggregateRule

---
+++ src/AggregateRule.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 15.08.19
+ * Time: 14:47
+ */
+
+namespace ScoutElastic;
+
+
+class AggregateRule
+{
+    public function buildAggregatePayload()
+    {
+        //
+    }
+}
