4fcc3319785f1bc52fa56150c8efdf1e148c4178
Alex Kovtunov <582969@gmail.com>
2019-09-04 18:12:00 +0400

    fix code style

--- src/AggregateRule.php
+++ src/AggregateRule.php
@@ -1,18 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alex
- * Date: 15.08.19
- * Time: 14:47
- */
 
 namespace ScoutElastic;
-
 
 class AggregateRule
 {
     public function buildAggregatePayload()
     {
-        //
     }
 }
