37bd2ce3cc1998489247280c552e7b58ec4338b3
Julien Arcin <1089816+julienarcin@users.noreply.github.com>
2019-12-29 00:43:53 +0700

    FIX compatibility with Laravel 6.8 (#307)
    Remove -s option for searchable model generation to fix compatibility with laravel 6.8

--- src/Console/SearchableModelMakeCommand.php
+++ src/Console/SearchableModelMakeCommand.php
@@ -41,7 +41,7 @@ class SearchableModelMakeCommand extends ModelMakeCommand
 
         $options[] = [
             'search-rule',
-            's',
+            null,
             InputOption::VALUE_REQUIRED,
             'Specify the search rule for the model. It\'ll be created if doesn\'t exist.',
         ];
